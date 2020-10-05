53d9ebf3e7ec84d0e6b1de8b60e12f04657a3851
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-10-05 13:09:41 -0400

    Initial commit
    Bump lodash from 4.17.15 to 4.17.20
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.15 to 4.17.20.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.15...4.17.20)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump prismjs from 1.20.0 to 1.21.0
    
    Bumps [prismjs](https://github.com/PrismJS/prism) from 1.20.0 to 1.21.0.
    - [Release notes](https://github.com/PrismJS/prism/releases)
    - [Changelog](https://github.com/PrismJS/prism/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/PrismJS/prism/compare/v1.20.0...v1.21.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump node-fetch from 2.6.0 to 2.6.1
    
    Bumps [node-fetch](https://github.com/bitinn/node-fetch) from 2.6.0 to 2.6.1.
    - [Release notes](https://github.com/bitinn/node-fetch/releases)
    - [Changelog](https://github.com/node-fetch/node-fetch/blob/master/docs/CHANGELOG.md)
    - [Commits](https://github.com/bitinn/node-fetch/compare/v2.6.0...v2.6.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    using typescript cuz of the clout factor 😎
    
    most of the grunt work is done, but the real challenge is deployment 🤔
    
    printing html to pdf is whack
    
    surprisingly easy to build 🤠
    
    when will @FortAwesome make the webcomponent :weary:
    
    I couldnt live with node_modules being in my repo, so i switched to a cdn 🤷‍♀️
    
    css grid never fails to dissapoint lol
    
    the co-op course in uni isn't the best :(
    
    gotta figure out cdn with webcomponents smh
    
    everytime the repo gets pushed, the updated at time changes wowo
    
    oops forgot to bundle
    
    html to pdf no no so yall just have to print to pdf :P
    
    yea no more printing, guess ill do it manually then :sigh:
    
    white is too boring
    
    english go brrr
    
    :sparkles: **commits** :sparkles:
    
    teeny tiny itty bitty fixer upper
    
    pls give job
    
    oompa loompa doopity doo
    
    when you forgot to compile typescript 🤦‍♂️
    
    idk why dependabot couldnt fix it
    
    Bump ini from 1.3.5 to 1.3.8
    
    Bumps [ini](https://github.com/isaacs/ini) from 1.3.5 to 1.3.8.
    - [Release notes](https://github.com/isaacs/ini/releases)
    - [Commits](https://github.com/isaacs/ini/compare/v1.3.5...v1.3.8)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    updates + refinements
    
    give job pls :((
    
    if ur an employer and ur seeing this, @navn-r :))
    
    typescript t r a n s p i l e d
    
    t w e a k s
    
    smol changes
    
    edit: renumber
    
    cgpa update
    
    automated print setup babyyy
    
    Update README.md
    
    fixed vulnerability
    
    delete vscode stuff
    
    Create node.js.yml
    
    Update node.js.yml
    
    Delete node.js.yml
    
    updated gitignore
    
    init travis
    
    testing deployment with macos
    
    updated config
    
    ts-node ftw
    
    ... trying stuff again lol
    
    booooooop
    
    Deploy navn-r/resume to github.com/navn-r/resume.git:master
    
    trying something...again
    
    idek i rly want this to work
    
    more a u t o m a t i o n
    
    Update README.md
    
    json resume noiceeeee
    
    wowo json resume ftw
    
    f a v i c o n
    
    Update resume.json
    
    Update experience-card.ts
    
    Update resume.json
    
    white line fix
    
    testing
    
    finally fixed puppeteer issue with white line
    
    pls hire me 🥺
    
    more material for you to hire me 🥰
    
    const-ant improvements, as usual
    
    Updated formatting for work experiences
    
    removed project dates from resume and added more styles to screen only
    
    Bump pug-code-gen from 2.0.2 to 2.0.3
    
    Bumps [pug-code-gen](https://github.com/pugjs/pug) from 2.0.2 to 2.0.3.
    - [Release notes](https://github.com/pugjs/pug/releases)
    - [Commits](https://github.com/pugjs/pug/compare/pug-code-gen@2.0.2...pug@2.0.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>

---
+++ src/components/header.ts
@@ -0,0 +1,199 @@
+import './icon';
+import {css, customElement, html, LitElement, property} from 'lit-element';
+
+export interface About {
+  firstName: string;
+  lastName: string;
+  contact: {
+    portfolio: string;
+    email: string;
+    linkedIn: string;
+    github: string;
+  }
+}
+
+@customElement('app-header')
+export class Header extends LitElement {
+  static styles = css`
+    #header {
+      background-color: var(--primary);
+      display: grid;
+      grid-template-columns: 2fr 1fr;
+      padding: 1.5rem;
+      color: #fff;
+    }
+
+    fa-icon {
+      font-size: 1.5rem;
+      margin-right: 1rem;
+      color: var(--accent);
+    }
+
+    #name h1 {
+      font-size: 3.5rem;
+      font-weight: 400;
+      font-style: italic;
+      text-align: center;
+    }
+
+    #lastName {
+      font-weight: 600;
+      color: var(--accent);
+    }
+
+    #contact-container {
+      display: grid;
+      grid-template-rows: auto;
+      row-gap: 10px;
+    }
+
+    .contact {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      width: 100%;
+    }
+
+    .contact a {
+      text-decoration: none;
+      color: #fff;
+    }
+
+    a {
+      text-decoration: none;
+      color: var(--accent);
+      outline: none;
+      border: 0;
+    }
+
+    .footer {
+      width: 100%;
+      display: flex;
+      justify-content: space-evenly;
+      background-color: var(--text);
+      color: var(--accent);
+      padding: 1rem 0;
+    }
+
+    .footer fa-icon {
+      font-size: 1rem;
+    }
+
+    
+    @media print {
+      .footer {
+        display: none;
+      }
+    }
+
+    @media screen and (max-width: 900px) {
+      @media (max-width: 900px) {
+        #header {
+          grid-template-columns: auto;
+          justify-content: center;
+        }
+
+        #contact-container {
+          grid-template-columns: repeat(4, 1fr);
+        }
+
+        .contact {
+          justify-content: center;
+          padding: 0 1rem;
+        }
+      }
+
+      @media (max-width: 600px) {
+        #header {
+          padding: 1rem;
+        }
+
+        #contact-container {
+          grid-template-columns: repeat(2, 1fr);
+        }
+
+        .contact {
+          justify-content: flex-start;
+        }
+
+        #name h1 {
+          font-size: 2.5rem;
+        }
+      }
+
+      @media (max-width: 375px) {
+        #contact-container {
+          grid-template-columns: auto;
+          margin-bottom: 1.5rem;
+        }
+      }
+    }
+  `;
+
+  @property({attribute: false})
+  time: Date = new Date(Date.now());
+
+  @property({type: Object})
+  about!: About;
+
+  constructor() {
+    super();
+    this.getRecentCommit();
+  }
+
+  getRecentCommit(): void {
+    fetch('https://api.github.com/repos/navn-r/resume')
+      .then((res) => res.json())
+      .then((repo) => {
+        this.time = new Date(repo.pushed_at);
+      });
+  }
+
+  render() {
+    return html`
+      <div id="header">
+        <div id="name">
+          <h1>${this.about.firstName} <span id="lastName">${this.about.lastName}</span></h1>
+        </div>
+        <div id="contact-container">
+          <div class="contact">
+            <fa-icon class="fas fa-link"></fa-icon>
+            <a href="https://${this.about.contact.portfolio}">${this.about.contact.portfolio}</a>
+          </div>
+          <div class="contact">
+            <fa-icon class="fas fa-paper-plane"></fa-icon>
+            <a href="mailto:${this.about.contact.email}"
+              >${this.about.contact.email}</a
+            >
+          </div>
+          <div class="contact">
+            <fa-icon class="fab fa-linkedin"></fa-icon>
+            <a href="https://linkedin.com/in/${this.about.contact.linkedIn}"
+              >${this.about.contact.linkedIn}</a
+            >
+          </div>
+          <div class="contact">
+            <fa-icon class="fab fa-github"></fa-icon>
+            <a href="https://github.com/${this.about.contact.github}">${this.about.contact.github}</a>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        <div style="display: flex; align-items: center;">
+          <em>Updated: ${this.time.toDateString()}</em>
+        </div>
+        <div>
+          <a href="./RavindaranNavinn_Resume.pdf" target="_blank">
+            Download PDF <fa-icon class="fas fa-download"></fa-icon>
+          </a>
+        </div>
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'app-header': Header;
+  }
+}
